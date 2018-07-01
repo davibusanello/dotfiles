7a2e27420a3639c83b3da45554b8f0c4c561a4a9
Davi Busanello <itsme@davibusanello.net>
2018-07-01 20:07:40 -0300

    Fix loader & composer alias

--- lib/aliases/loader.sh
+++ lib/aliases/loader.sh
@@ -1,2 +1,2 @@
 # for f in $DOTFILES_PATH/lib/aliases/load_*; do source $f; done
-for f in $(find $DOTFILES_PATH/lib/aliases -iname 'load_*.sh'); do echo $f; done
+for f in $(find $DOTFILES_PATH/lib/aliases -iname 'load_*.sh'); do source $f; done
