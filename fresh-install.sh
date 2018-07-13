42a35e0f332a3b5e5ba9353e94508e7fe5681692
Davi Busanello <itsme@davibusanello.net>
2018-07-12 23:49:20 -0300

    Add after docker installation setting up

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,5 +1,14 @@
-#Increasing the amount of inotify watchers
+echo 'Increasing the amount of inotify watchers'
 #Debian / RedHat...
 #echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 #Arch/Manjaro
 echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
+
+# After docker installation
+echo 'Setting up Docker user group & service'
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo 'Done! :D'
