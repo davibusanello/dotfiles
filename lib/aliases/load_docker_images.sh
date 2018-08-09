bf183c634ec4bcb649a13041cf6fcaed29345994
Davi Busanello <itsme@davibusanello.net>
2018-08-09 02:04:00 -0300

    Improved the docker aliases

---
+++ lib/aliases/load_docker_images.sh
@@ -0,0 +1,17 @@
+# List of images to use and set as defaults for aliases
+# Ready images
+# REPOSITORY:TAG
+# composer:1.6
+# php:5.6-fpm-alpine3.7
+# php:7.0-fpm-alpine3.7
+# php:7.0-alpine3.7
+# php:7.1-fpm-alpine3.7
+# php:7.1-alpine3.7
+# php:7.2-fpm-alpine3.7
+# php:7.2-alpine3.7
+# postgres:10-alpine
+
+
+# PHP ENV
+COMPOSER_DOCKER_IMAGE="composer:1.6"
+PHP_DOCKER_IMAGE="php:7.2-fpm-alpine3.7"
