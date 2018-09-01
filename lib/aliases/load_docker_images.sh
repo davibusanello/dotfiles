2b7ccd6c9409eadc0beb1fab97c68f29f577d238
Davi Busanello <itsme@davibusanello.net>
2018-09-01 02:05:14 -0300

    Add new images

--- lib/aliases/load_docker_images.sh
+++ lib/aliases/load_docker_images.sh
@@ -2,6 +2,7 @@
 # Ready images
 # REPOSITORY:TAG
 # composer:1.6
+# composer:1.7
 # php:5.6-fpm-alpine3.7
 # php:7.0-fpm-alpine3.7
 # php:7.0-alpine3.7
@@ -10,8 +11,15 @@
 # php:7.2-fpm-alpine3.7
 # php:7.2-alpine3.7
 # postgres:10-alpine
+# mongo:3.6-jessie
+# mongo:4.0-xenial
+# node:8.11-alpine
+# node:10.9-alpine
+# redis:4.0-alpine
+# ruby:2.5-alpine3.7
+# ruby:2.4-alpine3.7
 
 
 # PHP ENV
-COMPOSER_DOCKER_IMAGE="composer:1.6"
+COMPOSER_DOCKER_IMAGE="composer:1.7"
 PHP_DOCKER_IMAGE="php:7.2-fpm-alpine3.7"
