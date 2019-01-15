6c08d0ab8aad76bfce0203df4ce6d2803b24c0df
Davi Busanello <itsme@davibusanello.net>
2019-01-15 17:03:48 -0200

    Update files

--- shell/bin/backup.sh
+++ shell/bin/backup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -e
 # Simple script to backup MyData partition to external storage
 MACHINE="$(uname -n)"
 BACKUP_DATE="$(date +%Y-%m-%d)"
-rsync -avzh --progress /run/media/davi/MyData/* /run/media/davi/Seagate3T/Backups/$(MACHINE)-MyData-$(BACKUP_DATE)/
+DEST_PATH="/run/media/davi/Seagate3T/BackupsExtra"
+rsync -azh --progress /home/davi/* $DEST_PATH/$MACHINE-home-$BACKUP_DATE/
+
+rsync -azh --progress /run/media/davi/MyData/* $DEST_PATH/$MACHINE-MyData-$BACKUP_DATE/
