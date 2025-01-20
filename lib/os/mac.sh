405d63bcc813c8e6f4aa412a0458ad079e3248f8
Davi Busanello <itsme@davibusanello.net>
2025-01-20 10:06:58 -0300

    Clean ups and updates MacOS specifics

--- lib/os/mac.sh
+++ lib/os/mac.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
 # Libs brew
-# M1 probably
+# Determine Homebrew base path based on architecture
 if [ -d '/opt/homebrew/opt' ]; then
-    BREW_OPT_PATH='/opt/homebrew/opt';
+    BREW_BASE='/opt/homebrew' # Apple Silicon
+    BREW_OPT_PATH="$BREW_BASE/opt"
+elif [ -d '/usr/local/opt' ]; then
+    BREW_BASE='/usr/local' # Intel
+    BREW_OPT_PATH="$BREW_BASE/opt"
 fi
 
-# Intel
-if [ -d '/usr/local/opt' ]; then
-    BREW_OPT_PATH='/usr/local/opt';
-fi
+# Set up base Homebrew paths first
+export PATH="$BREW_BASE/bin:$BREW_BASE/sbin:$PATH"
 
-BREW_EXECUTABLES_PATH='/usr/local/sbin'
-#For pkg-config to find libpq you may need to set:
+# PKG configs
 export PKG_CONFIG_PATH="$BREW_OPT_PATH/libpq/lib/pkgconfig"
 LIBPQ_PATH="$BREW_OPT_PATH/libpq/bin"
 
-# Python 3.8 bin path
-MY_PYTHON_38_PATH="$HOME/Library/Python/3.8/bin"
-export PATH=$LIBPQ_PATH:$PATH:$MY_PYTHON_38_PATH:$BREW_OPT_PATH/bin
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1) --enable-yjit"
+# Build the PATH
+export PATH="$LIBPQ_PATH:$PATH"
+export PATH="$BREW_OPT_PATH/bin:$PATH"
+export PATH="$BREW_OPT_PATH/mysql-client/bin:$PATH"
+
+# Ruby configs
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl) --enable-yjit"
 export RUBY_YJIT_ENABLE=1
 # MySQL Client bin and libraries
 export PATH=$PATH:$BREW_OPT_PATH/mysql-client/bin
@@ -31,8 +35,8 @@ export PATH=$PATH:$BREW_OPT_PATH/mysql-client/bin
 # End libs brew
 
 # Load NVM
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"                                       # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 # How to identify architecture
 # Apple Intel x86_64
