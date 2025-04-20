b4aa569f91cef6e6ecbd7aa012f5d40a01cf8ef0
Davi Busanello <itsme@davibusanello.me>
2025-04-20 10:38:00 -0300

    Fixes autoloader order by explicitly loading load__utils.sh before other files

--- lib/aliases/loader.sh
+++ lib/aliases/loader.sh
@@ -3,6 +3,14 @@
 # Load aliases and utility functions
 # File names are load_<name>.sh or load_<name>.zsh
 # Exception for load__utils.sh which is used for utility functions, which needs to be loaded first
-for f in $(find $DOTFILES_PATH/lib/aliases -iname 'load_*.sh' -o -iname 'load_*.zsh'); do
-    source $f
+
+# Always source utils first as it contains general utility functions
+UTILS="$DOTFILES_PATH/lib/aliases/load__utils.sh"
+if [ -f "$UTILS" ]; then
+    source "$UTILS"
+fi
+
+# Now source the rest, excluding load__utils.sh
+for f in $(find $DOTFILES_PATH/lib/aliases \( -iname 'load_*.sh' -o -iname 'load_*.zsh' \) ! -name 'load__utils.sh'); do
+    source "$f"
 done
